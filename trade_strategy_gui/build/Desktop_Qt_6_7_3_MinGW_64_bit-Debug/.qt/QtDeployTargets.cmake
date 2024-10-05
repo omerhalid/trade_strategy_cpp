65ed6de79e46cccc4217152fff1b494a24f696c6
omerhalid <omerhalidcinar@gmail.com>
2024-10-05 21:58:21 +0200

    Stop tracking build folder

--- trade_strategy_gui/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_trade_strategy_gui_FILE C:/Users/katka/source/trade_strategy_cpp/trade_strategy_gui/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/trade_strategy_gui.exe)
-set(__QT_DEPLOY_TARGET_trade_strategy_gui_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_trade_strategy_gui_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
